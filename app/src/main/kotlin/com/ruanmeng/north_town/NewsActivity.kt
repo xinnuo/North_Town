03af955a3f0841992a5014b3df0c9b8c07b19191
xinnuo <416143467@qq.com>
2018-03-12 18:24:14 +0800

    update 03-12/18:24

--- app/src/main/kotlin/com/ruanmeng/north_town/NewsActivity.kt
+++ app/src/main/kotlin/com/ruanmeng/north_town/NewsActivity.kt
@@ -2,14 +2,56 @@ package com.ruanmeng.north_town
 
 import android.os.Bundle
 import com.ruanmeng.base.BaseActivity
+import com.ruanmeng.base.load_Linear
+import com.ruanmeng.base.refresh
+import com.ruanmeng.base.startActivity
+import com.ruanmeng.model.CommonData
+import kotlinx.android.synthetic.main.activity_news.*
+import kotlinx.android.synthetic.main.layout_empty.*
+import kotlinx.android.synthetic.main.layout_list.*
+import kotlinx.android.synthetic.main.layout_title_filter.*
+import net.idik.lib.slimadapter.SlimAdapter
 
 class NewsActivity : BaseActivity() {
+
+    private val list = ArrayList<Any>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
         setToolbarVisibility(false)
-
         init_title()
+
+        list.add(CommonData())
+        list.add(CommonData())
+        list.add(CommonData())
+        list.add(CommonData())
+        list.add(CommonData())
+        list.add(CommonData())
+        mAdapter.updateData(list)
+    }
+
+    override fun init_title() {
+        filter_check.setOnClickListener {
+            when (news_expand.isExpanded) {
+                true -> news_expand.collapse()
+                else -> news_expand.expand()
+            }
+        }
+
+        empty_hint.text = "暂无相关客户信息！"
+        swipe_refresh.refresh { getData(mPosition) }
+        recycle_list.load_Linear(baseContext, swipe_refresh) {
+            if (!isLoadingMore) {
+                isLoadingMore = true
+                getData(pageNum)
+            }
+        }
+
+        mAdapter = SlimAdapter.create()
+                .register<CommonData>(R.layout.item_data_list) { data, injector ->
+                    injector.clicked(R.id.item_data) { startActivity(NewsDetailActivity::class.java) }
+                }
+                .attachTo(recycle_list)
     }
 }
--- app/src/main/kotlin/com/ruanmeng/north_town/NewsDetailActivity.kt
+++ app/src/main/kotlin/com/ruanmeng/north_town/NewsDetailActivity.kt
@@ -2,6 +2,8 @@ package com.ruanmeng.north_town
 
 import android.os.Bundle
 import com.ruanmeng.base.BaseActivity
+import com.ruanmeng.base.startActivity
+import kotlinx.android.synthetic.main.activity_news_detail.*
 
 class NewsDetailActivity : BaseActivity() {
 
@@ -9,5 +11,17 @@ class NewsDetailActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_detail)
         init_title("客户详情", "添加订单")
+    }
+
+    @Suppress("DEPRECATION")
+    override fun init_title() {
+        super.init_title()
+        val drawable = resources.getDrawable(R.mipmap.nav_add)
+        // 这一步必须要做,否则不会显示
+        drawable.setBounds(0, 0, drawable.minimumWidth, drawable.minimumHeight)
+        tvRight.setCompoundDrawables(drawable, null, null, null)
+
+        tvRight.setOnClickListener { startActivity(ReportAddActivity::class.java) }
+        news_look.setOnClickListener { startActivity(DataCheckActivity::class.java) }
     }
 }
