ec3ebca8ecdb31ffaadd16fdf422d19ac2671b2b
xinnuo <416143467@qq.com>
2018-04-10 10:01:51 +0800

    update 04-10/10:01

--- app/src/main/java/com/ruanmeng/share/Const.java
+++ app/src/main/java/com/ruanmeng/share/Const.java
@@ -11,6 +11,6 @@ public class Const {
     public static final String SAVE_FILE = "/North_Town";
     public static final String MAKER = "1521105396";
     // APK下载地址
-    public static final String URL_DOWNLOAD = "https://www.pgyer.com/apiv2/app/install?appKey=45ae3b42719f70c7a45e5c831f856ace&_api_key=bdcb07efb995304f749c50ade9a8f7ad";
+    public static final String URL_DOWNLOAD = "https://www.pgyer.com/apiv2/app/install?appKey=9ad1602c1c7171c8f1f791d4f2c76d8c&_api_key=bdcb07efb995304f749c50ade9a8f7ad";
 
 }
